d1a57c026c3007c0a682e59f5ad183aaebee2854
Colin Ries <colin.ries@gmail.com>
2015-05-26 03:25:24 +0200

    Appendix
    Added timetables activity to navigation drawer

--- app/src/main/java/com/lumi_dos/lge/LGE.java
+++ app/src/main/java/com/lumi_dos/lge/LGE.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 
 public class LGE extends Application {
 
-    public static int ICONS[] = {R.drawable.home, R.drawable.intranet, R.drawable.news, R.drawable.contact, R.drawable.about, R.drawable.website, R.drawable.feedback};
+    public static int ICONS[] = {R.drawable.home, R.drawable.intranet, R.drawable.timetable, R.drawable.news, R.drawable.contact, R.drawable.about, R.drawable.website, R.drawable.feedback};
 
     public static Intent startActivityOnNavDrawerCAll(int childNumber, Context context, String feedback_address, String feedback_subject, String feedback_message, String choose_email_client) {
         if(childNumber == 1){
@@ -20,15 +20,17 @@ public class LGE extends Application {
         } else if(childNumber == 2) {
             return new Intent(context, IntranetActivity.class);
         } else if(childNumber == 3) {
-            return new Intent(context, NewsActivity.class);
+            return new Intent(context, TimetablesActivity.class);
         } else if(childNumber == 4) {
-            return new Intent(context, ContactActivity.class);
+            return new Intent(context, NewsActivity.class);
         } else if(childNumber == 5) {
-            return new Intent(context, AboutActivity.class);
+            return new Intent(context, ContactActivity.class);
         } else if(childNumber == 6) {
+            return new Intent(context, AboutActivity.class);
+        } else if(childNumber == 7) {
             Uri uri = Uri.parse("http://www.lge.lu");
             return new Intent(Intent.ACTION_VIEW, uri);
-        } else if(childNumber == 7) {
+        } else if(childNumber == 8) {
             Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                     "mailto", feedback_address, null));
 
